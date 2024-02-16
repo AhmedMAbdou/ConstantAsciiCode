bf8113f4fffe8245e2e03da1ebbd04c56c06bf0e
Abdou <ahmedmabdou10@gmail.com>
2024-02-16 09:36:15 +0200

    Add project files.

---
+++ CSharp.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            for (int i = 0; i < input.Length; i++)
+            {
+                Console.WriteLine($"{input[i]} → {(int)input[i]}");
+            }
+        }
+    }
+}
